36694466f2db55bced7859320ec66529389f8626
aarogacki <alainarogacki@gmail.com>
2020-09-05 20:14:21 -0400

    redo

---
+++ alainacodes/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteTitle: 'hello world',
+  manifestName: 'alainacodes',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-highlights/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'alaina',
+  subHeading: 'software engineer',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/alaina-codes',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/alainacodes',
+    },
+    {
+      icon: 'fa-linkedin',
+      name: 'LinkedIn',
+      url: 'https://linkedin.com/in/alainarogacki/',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:alaina.codes@gmail.com',
+    },
+  ],
+};
