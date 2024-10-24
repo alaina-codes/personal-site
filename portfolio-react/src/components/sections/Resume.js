ef3efeb8dc18237e084f5d2ceb211c78baad2422
Alaina Rogacki <alainarogacki@gmail.com>
2024-10-24 14:55:08 -0400

    add resume section for eventual popover maybe

---
+++ portfolio-react/src/components/sections/Resume.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Resume = () => {
+    return (
+        <section className="resume-section">
+            this will be a popover of my resume
+        </section>
+    );
+}
+
+export default Resume;
