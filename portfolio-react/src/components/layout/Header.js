9b5fe5f80e02339fa292426a1d255399654b9931
Alaina Rogacki <alainarogacki@gmail.com>
2024-10-24 13:56:42 -0400

    add placeholder components

---
+++ portfolio-react/src/components/layout/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="intro-header">
+      <h1>hi - i'm alaina, software engineer</h1>
+      <h2>welcome to my portfolio</h2>
+        <div className="header-buttons">
+            <button>Download CV</button>
+            <button>Contact Me</button>
+        </div>
+        <img src="path/to/headshot.jpg" alt="Alaina's headshot. Add more description once headshot is chosen" /> {/* Replace with actual path */}
+    </header>
+  );
+};
+
+export default Header;
+
+// notes
+// hi i'm alaina - software engineer
+// welcome to my portfolio
+
+// two buttons
+// download CV + contact me
+
+// headshot image
