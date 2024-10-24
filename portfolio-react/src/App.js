4165f4f69de76760d332f499e8a6b96e8a35a4a1
Alaina Rogacki <alainarogacki@gmail.com>
2024-10-24 14:06:48 -0400

    update app

--- portfolio-react/src/App.js
+++ portfolio-react/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
+import AboutMe from './components/sections/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
+      <Footer />
     </div>
   );
 }
